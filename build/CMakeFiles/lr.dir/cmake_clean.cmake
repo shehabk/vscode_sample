38e7c29fea67b8a54291c0f269cda83d6b783d71
Ahmed Shehab Khan <the.shihab.khan@gmail.com>
2018-02-27 22:38:48 -0500

    add

---
+++ build/CMakeFiles/lr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lr.dir/src/demo.cpp.o"
+  "lr.pdb"
+  "lr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<< "This is khan"<<endl;
+	cout<< "This is mir" <<endl;
+
+	int a = 54 + 6 ;
+	int b = 32 ;
+	
+	int c = a+b ;
+	cout << c <<endl ;
+
+ return 0;
+}
